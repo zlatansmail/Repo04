d1903c8d61c3566d584c8597df477ea9f18c23d6
zlatansmail <zlatan.s@outlook.com>
2021-09-16 15:22:04 +0200

    added package.json to exXY folders and exercise 4

---
+++ ex04/src/index.js
@@ -0,0 +1,18 @@
+function main() {
+
+    // Only change code bellow this line if you
+
+    myDecimal = 8.2;
+
+    myDecimalE = 0.00254;
+
+    //Only change code above this line
+
+    return {
+        myDecimal,
+        myDecimalE
+    };
+}
+
+console.log(main());
+module.exports = main;
