3a542fc0e4880e90bc9495c306c9bd6f6fcef0a4
zlatansmail <zlatan.s@outlook.com>
2021-09-16 20:38:40 +0200

    exercise 10

---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+    //Only change code bellow this line 
+    var myFirstName = "Zlatan";
+    var myLastName = "Smailbegovic";
+    var myHomeTown = "Sarajevo";
+
+
+    //only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
