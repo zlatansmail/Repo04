057c78e221450f03f1ca3ef40dc9002ea91ae6fc
zlatansmail <zlatan.s@outlook.com>
2021-09-16 14:58:09 +0200

    2nd exercese

---
+++ ex02/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    var myRes = 75;
+
+    // Only change code bellow this line if you
+
+    myRes++;
+
+
+    //Only change code above this line
+
+    return myRes;
+}
+
+console.log(main());
+module.exports = main;
