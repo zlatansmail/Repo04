f2b51d5d1d91e482be83965ee7c30aa8767f541b
zlatansmail <zlatan.s@outlook.com>
2021-09-16 15:43:19 +0200

    exercise 6

---
+++ ex06/src/index.js
@@ -0,0 +1,15 @@
+function main(numOne, numTwo) {
+
+    // Only change code bellow this line if you
+
+    myQuotient = numOne / numTwo;
+
+    //Only change code above this line
+
+    return myQuotient;
+}
+
+console.log(main(5.0, 2));
+console.log(main(16.5, 5.5));
+console.log(main(102.0, 25.5));
+module.exports = main;
